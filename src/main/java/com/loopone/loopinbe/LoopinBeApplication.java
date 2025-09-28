bc8a96a2f76ef68aade31d97a0fae95eb15cdf7b
koothemaverick <junhui9789@gmail.com>
2025-09-28 21:28:22 +0900

    Initial commit

---
+++ src/main/java/com/loopone/loopinbe/LoopinBeApplication.java
@@ -0,0 +1,13 @@
+package com.loopone.loopinbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoopinBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoopinBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loopone/loopinbe/LoopinBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loopone.loopinbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopinBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
