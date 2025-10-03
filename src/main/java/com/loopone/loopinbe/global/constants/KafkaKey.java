8f729d0e74c887a9cb0afaa1804d7142e8f35649
SSUN <rlatjsghks4647@naver.com>
2025-10-03 17:06:59 +0900

    feat : Constants key 추가

---
+++ src/main/java/com/loopone/loopinbe/global/constants/KafkaKey.java
@@ -0,0 +1,6 @@
+package com.loopone.loopinbe.global.constants;
+
+public class KafkaKey {
+    public static final String OPEN_AI_TOPIC = "openai-requests";
+    public static final String OPEN_AI_GROUP_ID = "openai-worker";
+}
---
+++ src/main/java/com/loopone/loopinbe/global/constants/RedisKey.java
@@ -0,0 +1,5 @@
+package com.loopone.loopinbe.global.constants;
+
+public class RedisKey {
+    public static final String OPEN_AI_RESULT_KEY = "openai:result:";
+}
