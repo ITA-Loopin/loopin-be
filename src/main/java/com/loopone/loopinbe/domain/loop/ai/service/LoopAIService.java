8ffede37061b2e81476eb9d446c325f0d78ee518
SSUN <rlatjsghks4647@naver.com>
2025-10-08 00:51:50 +0900

    feat : OpenAI 기능 구현

--- src/main/java/com/loopone/loopinbe/domain/loop/ai/service/LoopAIService.java
+++ src/main/java/com/loopone/loopinbe/domain/loop/ai/service/LoopAIService.java
@@ -3,5 +3,7 @@ package com.loopone.loopinbe.domain.loop.ai.service;
 import com.loopone.loopinbe.global.kafka.event.ai.AiRequestPayload;
 
 public interface LoopAIService {
+    String getAIResult(String requestId);
+
     String chat(AiRequestPayload message);
 }
