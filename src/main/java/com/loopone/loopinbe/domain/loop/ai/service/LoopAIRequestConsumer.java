073c90d6ddd422f34549eb003795b398400a20af
SSUN <rlatjsghks4647@naver.com>
2025-10-10 18:29:49 +0900

    remove : 불필요한 리스너, 프로바이더 제거

--- src/main/java/com/loopone/loopinbe/domain/loop/ai/service/LoopAIRequestConsumer.java
+++
@@ -1,7 +0,0 @@
-package com.loopone.loopinbe.domain.loop.ai.service;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-public interface LoopAIRequestConsumer {
-    void consume(String message) throws JsonProcessingException;
-}
