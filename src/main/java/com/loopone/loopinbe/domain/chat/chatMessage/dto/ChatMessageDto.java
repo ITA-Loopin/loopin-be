a3d0503a6d71068c25bf56390a31e7013997057e
SSUN <96679633+SeonHwan-Kim@users.noreply.github.com>
2025-10-31 18:24:06 +0900

    [Feat/#22] AI 루프 추천 구현 (#225)
    * feat : LoopAI Producer, Consumer 구현
    
    * feat : LoopAI Controller 구현
    
    * remove : LoopAIController 주석처리
    
    * feat : OpenAI 기능 구현
    
    * remove : 불필요한 리스너, 프로바이더 제거
    
    * refactor : getAIResult  제거
    
    * feat : loop 추천 prompt 추가
    
    * feat : ai 추천 루프 response dto추가
    
    * feat : 채팅 response에 recommendations 추가
    
    * feat : Consant에 AI_RESPONSE_MESSAGE 추가
    
    * feat : OpenAI 반환값 RecommendationsLoop 형식으로 변환
    
    * refactor : AiEventListener에서 직접 브로드캐스트로 수정
    
    * Fix/#223 (#224)
    
    * fix : MessageContent에 recommendations 추가
    
    * fix : mongo upsert에 recommendations 추가
    
    * fix : ChatMessageDto tempId 추가
    
    * fix : AI 답변 흐름 변경

--- src/main/java/com/loopone/loopinbe/domain/chat/chatMessage/dto/ChatMessageDto.java
+++ src/main/java/com/loopone/loopinbe/domain/chat/chatMessage/dto/ChatMessageDto.java
@@ -1,12 +1,14 @@
 package com.loopone.loopinbe.domain.chat.chatMessage.dto;
 
 import com.loopone.loopinbe.domain.chat.chatMessage.entity.ChatMessage;
+import com.loopone.loopinbe.domain.loop.loop.dto.req.LoopCreateRequest;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @Builder
@@ -14,11 +16,13 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class ChatMessageDto {
     private Long id;
+    private String tempId;
     private Long chatRoomId;
     private Long memberId;
     private String nickname;
     private String profileImageUrl;
     private String content;
     ChatMessage.AuthorType authorType;
+    private List<LoopCreateRequest> recommendations;
     private LocalDateTime createdAt;
 }
--- src/main/java/com/loopone/loopinbe/global/kafka/event/ai/AiEventListener.java
+++ src/main/java/com/loopone/loopinbe/global/kafka/event/ai/AiEventListener.java
@@ -2,16 +2,22 @@ package com.loopone.loopinbe.global.kafka.event.ai;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.loopone.loopinbe.domain.chat.chatMessage.dto.ChatInboundMessagePayload;
+import com.loopone.loopinbe.domain.chat.chatMessage.dto.ChatMessageDto;
 import com.loopone.loopinbe.domain.chat.chatMessage.entity.ChatMessage;
+import com.loopone.loopinbe.domain.chat.chatMessage.service.ChatMessageService;
+import com.loopone.loopinbe.domain.loop.ai.dto.res.RecommendationsLoop;
 import com.loopone.loopinbe.domain.loop.ai.service.LoopAIService;
 import com.loopone.loopinbe.global.exception.ServiceException;
 import com.loopone.loopinbe.global.kafka.event.chatMessage.ChatMessageEventPublisher;
+import com.loopone.loopinbe.global.webSocket.handler.ChatWebSocketHandler;
+import com.loopone.loopinbe.global.webSocket.payload.ChatWebSocketPayload;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import static com.loopone.loopinbe.global.constants.Constant.AI_RESPONSE_MESSAGE;
 import static com.loopone.loopinbe.global.constants.KafkaKey.*;
 
 @Slf4j
@@ -21,6 +27,8 @@ public class AiEventListener {
     private final ObjectMapper objectMapper;
     private final ChatMessageEventPublisher chatMessageEventPublisher; // send-message-topic 발행기
     private final LoopAIService loopAIService;
+    private final ChatWebSocketHandler chatWebSocketHandler;
+    private final ChatMessageService chatMessageService;
 
     @KafkaListener(
             topics = OPEN_AI_TOPIC,
@@ -32,20 +40,42 @@ public class AiEventListener {
             AiRequestPayload req = objectMapper.readValue(rec.value(), AiRequestPayload.class);
 
             // 1) 프롬프트 구성 + LLM 호출
-            String loopRecommend = loopAIService.chat(req);
+            RecommendationsLoop loopRecommend = loopAIService.chat(req);
 
             // 2) 여기서 AI 답변용 ChatInboundMessagePayload 생성
             ChatInboundMessagePayload botInbound = new ChatInboundMessagePayload(
                     deterministicMessageKey(req),       // 멱등키 (아래 참고)
                     req.chatRoomId(),
                     null,
-                    loopRecommend,
+                    AI_RESPONSE_MESSAGE,
+                    loopRecommend.recommendations(),
                     ChatMessage.AuthorType.BOT,
                     java.time.LocalDateTime.now()
             );
 
-            // 3) 기존 파이프라인 재사용: send-message-topic 으로 재주입
-            chatMessageEventPublisher.publishInbound(botInbound);
+            // 3) 브로드캐스트
+            ChatMessageDto resp = ChatMessageDto.builder()
+                    .tempId(botInbound.messageKey())
+                    .chatRoomId(botInbound.chatRoomId())
+                    .memberId(botInbound.memberId())
+                    .content(botInbound.content())
+                    .recommendations(botInbound.recommendations())
+                    .authorType(botInbound.authorType())
+                    .createdAt(botInbound.createdAt() != null
+                            ? botInbound.createdAt()
+                            : null)
+                    .build();
+
+            ChatWebSocketPayload out = ChatWebSocketPayload.builder()
+                    .messageType(ChatWebSocketPayload.MessageType.MESSAGE)
+                    .chatRoomId(botInbound.chatRoomId())
+                    .chatMessageDto(resp)
+                    .lastMessageCreatedAt(resp.getCreatedAt())
+                    .build();
+            chatWebSocketHandler.broadcastToRoom(botInbound.chatRoomId(), objectMapper.writeValueAsString(out));
+
+            // 4) RDB 저장
+            chatMessageService.processInbound(botInbound);
         } catch (ServiceException se) {
             log.warn("AI biz error: {}", se.getReturnCode(), se);
             throw se; // not-retry → DLT
