9fa1aab1b85ba420b4265feaa5f12f9e372e5b08
dbalsk <dbalsrltk@naver.com>
2025-10-31 18:06:41 +0900

    chore: 그룹id nullable=false 제거 (단일 루프인 경우 오류 발생)

--- src/main/java/com/loopone/loopinbe/domain/loop/loop/dto/req/LoopCreateRequest.java
+++ src/main/java/com/loopone/loopinbe/domain/loop/loop/dto/req/LoopCreateRequest.java
@@ -3,7 +3,6 @@ package com.loopone.loopinbe.domain.loop.loop.dto.req;
 import com.loopone.loopinbe.domain.loop.loop.enums.RepeatType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.DayOfWeek;
