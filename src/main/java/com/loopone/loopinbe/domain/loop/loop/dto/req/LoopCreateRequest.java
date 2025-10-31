33908e99a60362ce4e9dd27efbc5a38c37489913
dbalsk <dbalsrltk@naver.com>
2025-10-31 15:50:52 +0900

    fix: LoopCreateRequest에 반복 규칙 추가 및 수정

--- src/main/java/com/loopone/loopinbe/domain/loop/loop/dto/req/LoopCreateRequest.java
+++ src/main/java/com/loopone/loopinbe/domain/loop/loop/dto/req/LoopCreateRequest.java
@@ -1,5 +1,6 @@
 package com.loopone.loopinbe.domain.loop.loop.dto.req;
 
+import com.loopone.loopinbe.domain.loop.loop.enums.RepeatType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
@@ -11,10 +12,30 @@ import java.util.List;
 
 @Schema(description = "루프 생성을 위한 요청 DTO")
 public record LoopCreateRequest (
-    @NotBlank String title,
-    String content,
-    @NotEmpty List<DayOfWeek> daysOfWeek,
-    @NotNull(message = "시작일은 반드시 지정해야 합니다.") LocalDate startDate,
-    LocalDate endDate,
-    List<String> checklists //추가할 체크리스트
+        @Schema(description = "루프 제목")
+        @NotBlank
+        String title,
+
+        @Schema(description = "루프 내용")
+        String content,
+
+        @Schema(description = "반복 규칙 (NONE, WEEKLY, MONTHLY, YEARLY)")
+        @NotNull
+        RepeatType scheduleType,
+
+        @Schema(description = "scheduleType이 NONE일 때 사용할 특정 날짜")
+        LocalDate specificDate,
+
+        @Schema(description = "scheduleType이 WEEKLY일 때 사용할 요일 목록")
+        List<DayOfWeek> daysOfWeek,
+
+        @Schema(description = "반복 시작일 (WEEKLY, MONTHLY, YEARLY일 때 사용, 기본값은 당일로 설정)")
+        LocalDate startDate,
+
+        @Schema(description = "반복 종료일 (WEEKLY, MONTHLY, YEARLY일 때 사용, null이면 1년 후로 설정)")
+        LocalDate endDate,
+
+        @Schema(description = "각 루프에 포함될 체크리스트 내용 목록")
+        List<String> checklists
+
 ){}
