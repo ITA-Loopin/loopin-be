1accb3e6367f7acc0769d2081c02189231de1563
dbalsk <dbalsrltk@naver.com>
2025-11-08 15:37:17 +0900

    feat: 루프 그룹 전체 수정 컨트롤러 메서드 구현

--- src/main/java/com/loopone/loopinbe/domain/loop/loop/controller/ApiV1LoopController.java
+++ src/main/java/com/loopone/loopinbe/domain/loop/loop/controller/ApiV1LoopController.java
@@ -3,6 +3,7 @@ package com.loopone.loopinbe.domain.loop.loop.controller;
 import com.loopone.loopinbe.domain.account.auth.currentUser.CurrentUser;
 import com.loopone.loopinbe.domain.account.auth.currentUser.CurrentUserDto;
 import com.loopone.loopinbe.domain.loop.loop.dto.req.LoopCreateRequest;
+import com.loopone.loopinbe.domain.loop.loop.dto.req.LoopGroupUpdateRequest;
 import com.loopone.loopinbe.domain.loop.loop.dto.req.LoopUpdateRequest;
 import com.loopone.loopinbe.domain.loop.loop.dto.res.DailyLoopsResponse;
 import com.loopone.loopinbe.domain.loop.loop.dto.res.LoopDetailResponse;
@@ -87,7 +88,18 @@ public class ApiV1LoopController {
         return ApiResponse.success();
     }
 
-    //TODO: 그룹 전체 루프 수정 API 구현
+    //TODO: 루프 그룹 전체 수정 API 구현
+    @PutMapping("/loops/{loopGroup}")
+    @Operation(summary = "루프 그룹 전체 수정", description = "해당 그룹의 루프 전체를 수정합니다.")
+    public ApiResponse<Void> updateGroupLoop(
+            @Parameter(description = "수정할 루프 그룹의 ID") @PathVariable Long loopGroup,
+            @RequestBody @Valid LoopGroupUpdateRequest loopGroupUpdateRequest,
+            @Parameter(hidden = true) @CurrentUser CurrentUserDto currentUser
+    ){
+        loopService.updateLoopGroup(loopGroup, loopGroupUpdateRequest, currentUser);
+        return ApiResponse.success();
+    }
+
 
     //루프 삭제
     @DeleteMapping("/loops/{loopId}")
