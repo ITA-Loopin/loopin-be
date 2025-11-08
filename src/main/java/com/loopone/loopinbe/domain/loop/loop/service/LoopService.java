61992d5ca026344c7f2069b3667c896836f0391c
dbalsk <dbalsrltk@naver.com>
2025-11-08 15:36:43 +0900

    feat: 루프 그룹 전체 수정 서비스 메서드 추가

--- src/main/java/com/loopone/loopinbe/domain/loop/loop/service/LoopService.java
+++ src/main/java/com/loopone/loopinbe/domain/loop/loop/service/LoopService.java
@@ -2,6 +2,7 @@ package com.loopone.loopinbe.domain.loop.loop.service;
 
 import com.loopone.loopinbe.domain.account.auth.currentUser.CurrentUserDto;
 import com.loopone.loopinbe.domain.loop.loop.dto.req.LoopCreateRequest;
+import com.loopone.loopinbe.domain.loop.loop.dto.req.LoopGroupUpdateRequest;
 import com.loopone.loopinbe.domain.loop.loop.dto.req.LoopUpdateRequest;
 import com.loopone.loopinbe.domain.loop.loop.dto.res.DailyLoopsResponse;
 import com.loopone.loopinbe.domain.loop.loop.dto.res.LoopDetailResponse;
@@ -26,6 +27,9 @@ public interface LoopService {
 
     //루프 수정
     void updateLoop(Long loopId, LoopUpdateRequest loopUpdateRequest, CurrentUserDto currentUser);
+
+    //루프 그룹 전체 수정
+    void updateLoopGroup(Long loopGroup, LoopGroupUpdateRequest loopGroupUpdateRequest, CurrentUserDto currentUser);
 
     //루프 삭제
     void deleteLoop(Long loopId, CurrentUserDto currentUser);
