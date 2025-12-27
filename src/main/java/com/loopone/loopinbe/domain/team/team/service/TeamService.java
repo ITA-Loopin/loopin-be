a4648c229050d0c6acff05a5c681640f25146084
dbalsk <dbalsrltk@naver.com>
2025-12-27 13:20:32 +0900

    feat: Team 서비스 추가

--- src/main/java/com/loopone/loopinbe/domain/team/team/service/TeamService.java
+++ src/main/java/com/loopone/loopinbe/domain/team/team/service/TeamService.java
@@ -1,4 +1,15 @@
 package com.loopone.loopinbe.domain.team.team.service;
 
+
+import com.loopone.loopinbe.domain.account.auth.currentUser.CurrentUserDto;
+import com.loopone.loopinbe.domain.team.team.dto.req.TeamCreateRequest;
+import com.loopone.loopinbe.domain.team.team.dto.res.MyTeamResponse;
+import com.loopone.loopinbe.domain.team.team.dto.res.RecruitingTeamResponse;
+
+import java.util.List;
+
 public interface TeamService {
+    Long createTeam(TeamCreateRequest request, CurrentUserDto currentUser);
+    List<MyTeamResponse> getMyTeams(CurrentUserDto currentUser);
+    List<RecruitingTeamResponse> getRecruitingTeams(CurrentUserDto currentUser);
 }
