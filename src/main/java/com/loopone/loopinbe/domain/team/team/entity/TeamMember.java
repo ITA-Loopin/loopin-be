c07498011ccfcc28acdbe9127d29052dc385c044
dbalsk <dbalsrltk@naver.com>
2025-12-27 13:20:13 +0900

    feat: TeamMember 엔티티 생성

--- src/main/java/com/loopone/loopinbe/domain/team/team/entity/TeamMember.java
+++ src/main/java/com/loopone/loopinbe/domain/team/team/entity/TeamMember.java
@@ -1,4 +1,31 @@
 package com.loopone.loopinbe.domain.team.team.entity;
 
-public class TeamMember {
+import com.loopone.loopinbe.domain.account.member.entity.Member;
+import com.loopone.loopinbe.global.jpa.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class TeamMember extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 }
