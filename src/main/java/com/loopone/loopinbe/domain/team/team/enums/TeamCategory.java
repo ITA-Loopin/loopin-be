9bb79dd2b84ebd4dd0cc1b2ac8e6a8f60909cad1
dbalsk <dbalsrltk@naver.com>
2025-12-27 12:52:37 +0900

    feat: TeamCategory enum 추가

--- src/main/java/com/loopone/loopinbe/domain/team/team/enums/TeamCategory.java
+++ src/main/java/com/loopone/loopinbe/domain/team/team/enums/TeamCategory.java
@@ -1,4 +1,16 @@
 package com.loopone.loopinbe.domain.team.team.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum TeamCategory {
+    PROJECT("팀프로젝트"),
+    CONTEST("공모전"),
+    STUDY("스터디"),
+    ROUTINE("루틴 공유"),
+    ETC("기타");
+
+    private final String description;
 }
