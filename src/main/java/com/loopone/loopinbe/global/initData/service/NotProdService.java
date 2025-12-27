8e86aa964f75bd7dd6a7ab63b9025b4ce52f8ae1
구준휘 <121041290+KuJunhui@users.noreply.github.com>
2025-12-27 16:01:23 +0900

    [Chore/#390] ddl-auto -> create (#401)
    * feat: LoopReport 도메인 구현 완료, 테스트 시작
    
    - loopReport
    - initData
    
    * feat: LoopReport 도메인 테스트 완료
    
    - loopReport
    
    * chore: ddl-auto -> create

--- src/main/java/com/loopone/loopinbe/global/initData/service/NotProdService.java
+++ src/main/java/com/loopone/loopinbe/global/initData/service/NotProdService.java
@@ -5,7 +5,6 @@ import com.loopone.loopinbe.global.initData.member.service.NotProdMemberService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
