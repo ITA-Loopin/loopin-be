39b98c2e218be4394266538c5f424eeb7ca1474e
dbalsk <dbalsrltk@naver.com>
2025-12-30 21:31:50 +0900

    feat: TeamLoopRepository 구현

--- src/main/java/com/loopone/loopinbe/domain/team/teamLoop/repository/TeamLoopRepository.java
+++ src/main/java/com/loopone/loopinbe/domain/team/teamLoop/repository/TeamLoopRepository.java
@@ -1,4 +1,30 @@
 package com.loopone.loopinbe.domain.team.teamLoop.repository;
 
-public class TeamLoopRepository {
+import com.loopone.loopinbe.domain.team.team.entity.Team;
+import com.loopone.loopinbe.domain.team.teamLoop.entity.TeamLoop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
+
+
+public interface TeamLoopRepository extends JpaRepository<TeamLoop, Long> {
+
+    List<TeamLoop> findByTeamAndLoopdate(Team team, LocalDate loopDate);
+
+    @Query("""
+        SELECT tl
+        FROM TeamLoop tl
+        WHERE tl.team.id = :teamId AND tl.loopDate = :date
+        ORDER BY
+            CASE tl.importance
+                WHEN 'HIGH' THEN 1
+                WHEN 'MIDDLE' THEN 2
+                WHEN 'LOW' THEN 3
+            END ASC,
+            tl.id DESC
+    """)
+    List<TeamLoop> findAllByTeamIdAndDate(@Param("teamId") Long teamId, @Param("date") LocalDate date);
 }
