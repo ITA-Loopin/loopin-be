550cbddea7f36b38babc6e27ae7131958c977951
koothemaverick <junhui9789@gmail.com>
2026-01-02 02:54:03 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -106,7 +106,7 @@ resource "aws_security_group" "sg_1" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = var.admin_allowed_cidrs
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # NPM 접속
