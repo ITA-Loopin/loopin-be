5ca43063fae7b122fe2eb61ed2d4f7120db6f200
SSUN <rlatjsghks4647@naver.com>
2026-01-03 13:52:53 +0900

    fix : cors 문제 해결

--- src/main/java/com/loopone/loopinbe/global/config/WebConfig.java
+++ src/main/java/com/loopone/loopinbe/global/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.loopone.loopinbe.domain.account.auth.currentUser.CurrentUserArgumentR
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,5 +18,21 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(currentUserArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns(
+                        "http://localhost:8080",
+                        "http://localhost:3000",
+                        "http://local.loopin.co.kr",
+                        "https://local.loopin.co.kr",
+                        "https://loopin.co.kr",
+                        "https://develop.loopin.co.kr"
+                )
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("Authorization", "Content-Type", "Cache-Control", "Content-Length", "*")
+                .allowCredentials(true);
     }
 }
