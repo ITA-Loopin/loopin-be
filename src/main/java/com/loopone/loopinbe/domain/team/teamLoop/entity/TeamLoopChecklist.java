263fd0641fa7ce0171c65485e8325478aa071c61
dbalsk <dbalsrltk@naver.com>
2026-01-03 15:23:39 +0900

    feat: TeamLoopChecklist 엔티티에 owner 필드 추가

--- src/main/java/com/loopone/loopinbe/domain/team/teamLoop/entity/TeamLoopChecklist.java
+++ src/main/java/com/loopone/loopinbe/domain/team/teamLoop/entity/TeamLoopChecklist.java
@@ -1,5 +1,6 @@
 package com.loopone.loopinbe.domain.team.teamLoop.entity;
 
+import com.loopone.loopinbe.domain.account.member.entity.Member;
 import com.loopone.loopinbe.domain.team.team.entity.Team;
 import com.loopone.loopinbe.global.jpa.BaseEntity;
 import jakarta.persistence.*;
@@ -24,6 +25,15 @@ public class TeamLoopChecklist extends BaseEntity {
     @JoinColumn(name = "team_loop_id")
     private TeamLoop teamLoop;
 
+    // 공통(COMMON) -> null (팀장만 허용), 개인(INDIVIDUAL) -> owner 값인 멤버만 허용
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_member_id", nullable = true)
+    private Member owner;
+
     @Column(nullable = false, length = 200)
     private String content;
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
 }
