551bd2a06104c56ecc72170fe0dc954121b05bdb
dbalsk <dbalsrltk@naver.com>
2026-01-08 00:40:53 +0900

    fix: 체크리스트 생성 api 호출 시 응답 추가

--- src/main/java/com/loopone/loopinbe/domain/loop/loopChecklist/service/LoopChecklistService.java
+++ src/main/java/com/loopone/loopinbe/domain/loop/loopChecklist/service/LoopChecklistService.java
@@ -3,10 +3,11 @@ package com.loopone.loopinbe.domain.loop.loopChecklist.service;
 import com.loopone.loopinbe.domain.account.auth.currentUser.CurrentUserDto;
 import com.loopone.loopinbe.domain.loop.loopChecklist.dto.req.LoopChecklistCreateRequest;
 import com.loopone.loopinbe.domain.loop.loopChecklist.dto.req.LoopChecklistUpdateRequest;
+import com.loopone.loopinbe.domain.loop.loopChecklist.dto.res.LoopChecklistResponse;
 
 public interface LoopChecklistService {
     //체크리스트 생성
-    void addLoopChecklist(Long loopId, LoopChecklistCreateRequest loopChecklistCreateRequest, CurrentUserDto currentUser);
+    LoopChecklistResponse addLoopChecklist(Long loopId, LoopChecklistCreateRequest loopChecklistCreateRequest, CurrentUserDto currentUser);
 
     //체크리스트 수정
     void updateLoopChecklist(Long checkListId, LoopChecklistUpdateRequest loopChecklistUpdateRequest, CurrentUserDto currentUser);
