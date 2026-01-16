12238757f931bb8c031b972feee8dbb1aeb89296
구준휘 <121041290+KuJunhui@users.noreply.github.com>
2026-01-16 23:51:49 +0900

    feat: 팀 생성시 자동으로 초대 전송 및 초대 수락시 자동으로 채팅방 참여 (#591)

--- src/main/java/com/loopone/loopinbe/domain/team/team/dto/req/TeamInvitationCreateRequest.java
+++ src/main/java/com/loopone/loopinbe/domain/team/team/dto/req/TeamInvitationCreateRequest.java
@@ -6,11 +6,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class TeamInvitationCreateRequest {
     @NotNull(message = "초대할 멤버 ID는 필수입니다.")
-    private Long inviteeId;
+    private List<Long> inviteeIds;
 }
