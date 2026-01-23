d311c286d23581a4216af1d170b328f395d01cb0
koothemaverick <junhui9789@gmail.com>
2026-01-23 22:05:59 +0900

    공지 기능 구현

--- src/main/java/com/loopone/loopinbe/domain/chat/chatRoom/converter/ChatRoomConverter.java
+++ src/main/java/com/loopone/loopinbe/domain/chat/chatRoom/converter/ChatRoomConverter.java
@@ -12,21 +12,24 @@ import java.util.List;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ChatRoomConverter {
-    // ---------- ChatRoom -> ChatRoomResponse ----------
+    // ---------- ChatRoomMember -> ChatRoomResponse ----------
     @Mapping(target = "id", source = "chatRoom.id")
     @Mapping(target = "ownerId", source = "chatRoom.member.id")
     @Mapping(target = "title", source = "chatRoom.title")
     @Mapping(target = "loopSelect", expression = "java(chatRoomMember.getChatRoom().getLoop() != null)")
     @Mapping(target = "isCallUpdateLoop", source = "chatRoom.callUpdateLoop")
+    @Mapping(target = "noticeMessageContent", source = "chatRoom.noticeMessageContent")
     @Mapping(target = "lastMessageAt", source = "chatRoom.lastMessageAt")
     @Mapping(target = "lastReadAt", source = "lastReadAt")
     ChatRoomResponse toChatRoomResponse(ChatRoomMember chatRoomMember);
 
+    // ---------- ChatRoom -> ChatRoomResponse ----------
     @Mapping(target = "id", source = "id")
     @Mapping(target = "ownerId", source = "member.id")
     @Mapping(target = "title", source = "title")
     @Mapping(target = "loopSelect", expression = "java(chatRoom.getLoop() != null)")
     @Mapping(target = "isCallUpdateLoop", source = "callUpdateLoop")
+    @Mapping(target = "noticeMessageContent", source = "noticeMessageContent")
     @Mapping(target = "lastMessageAt", source = "lastMessageAt")
     @Mapping(target = "lastReadAt", ignore = true)
     ChatRoomResponse toChatRoomResponse(ChatRoom chatRoom);
