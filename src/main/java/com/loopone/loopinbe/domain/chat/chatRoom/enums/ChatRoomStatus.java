a8ee434d6357ba3f404795c9af8e19c6f7d6922e
SSUN <rlatjsghks4647@naver.com>
2026-01-27 22:53:57 +0900

    feat : chatRoom에 ChatRoomStatus 추가

---
+++ src/main/java/com/loopone/loopinbe/domain/chat/chatRoom/enums/ChatRoomStatus.java
@@ -0,0 +1,9 @@
+package com.loopone.loopinbe.domain.chat.chatRoom.enums;
+
+public enum ChatRoomStatus {
+    DEFAULT,
+    AFTER_CREATE_LOOP,
+    BEFORE_CLICK_UPDATE_LOOP,
+    AFTER_CLICK_UPDATE_LOOP,
+    AFTER_CREATE_UPDATE_LOOP
+}
